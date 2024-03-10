fc7a76f5e5dc8d040bbd709f38027dc736b3e85b
unknown <noufiyashefi@gmail.com>
2024-03-10 13:09:35 +0530

    quiz app first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const data = [
+    {
+        question: "Which of the following is used in React.js to increase performance",
+        option1: "Virtual DOM",
+        option2: "Real DOM",
+        option3: "Both A and B",
+        option4: "None of the Above",
+        ans: 1,
+    },
+    {
+        question: "Identify the one which is used to pass data to components from outside",
+        option1: "Render with arguments",
+        option2: "setState",
+        option3: "PropTypes",
+        option4: "props",
+        ans: 4,
+    },
+    {
+        question: "In which language is React.js written",
+        option1: "Python",
+        option2: "JavaScript",
+        option3: "Java",
+        option4: "PHP",
+        ans: 2,
+    },{
+        question: "What is ReactJS mainly used for building",
+        option1: "Database",
+        option2: "Connectivity",
+        option3: "User Interface",
+        option4: "Design Platform",
+        ans: 3,
+    },{
+        question: "Among the following which is used to create a class inheritance",
+        option1: "This",
+        option2: "Create",
+        option3: "Inherits",
+        option4: "Extends",
+        ans: 4,
+    }
+]
